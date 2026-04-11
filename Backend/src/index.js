dfaae5362760eae1ae99fd9282b99554e70305e9
Arindam Dwivedi <arindamdwivedi39@gmail.com>
2026-04-11 16:02:51 +0530

    Setup Backend Server and Initialised Database

---
+++ Backend/src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+// import productRoutes from "./routes/productRoutes.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Set up routes
+// app.use('/api/products', productRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
